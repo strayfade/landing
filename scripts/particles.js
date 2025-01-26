e119db1e85cb774b28361ebed38dafb735d61146
strayfade <64668046+strayfade@users.noreply.github.com>
2025-01-26 13:49:15 -0500

    Pressure gradient

--- scripts/particles.js
+++ scripts/particles.js
@@ -6,7 +6,9 @@ let size = {
     height: window.innerHeight
 }
 let maxReach = 200;
+let pointRadius = 0
 const renderPoints = () => {
+    pointRadius = 5
 
     if (size.width != window.innerWidth || size.height != window.innerHeight) {
         reloadPoints();
@@ -22,23 +24,26 @@ const renderPoints = () => {
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, size.width, size.height)
 
-    function drawPoint(x, y, radius = 50) {
-        radius = maxReach / 5
+    function drawPoint(x, y, color = "white") {
         x /= 100 / canvasRenderPercentage
         y /= 100 / canvasRenderPercentage
-        ctx.fillStyle = "white";
+        ctx.fillStyle = color;
         ctx.beginPath();
-        ctx.arc(x, y, radius * canvasRenderPercentage / 100, 0, Math.PI * 2);
+        ctx.arc(x, y, pointRadius * canvasRenderPercentage / 100, 0, Math.PI * 2);
         ctx.fill();
     }
 
     // Render the gradients
-    points.forEach(({ x, y }) => {
-        drawPoint(x, y);
+    points.forEach(({ x, y, color }) => {
+        drawPoint(x, y, color);
     });
 }
 
 const updatePoints = () => {
+    const mouseGradientPoint = {
+        x: mouseX,
+        y: mouseY
+    }
     for (let i = 0; i < points.length; i++) {
 
         const updatePoint = (atIndex) => {
@@ -55,20 +60,16 @@ const updatePoints = () => {
                     gradient = points[z]
                 }
                 else {
-                    gradient = {
-                        x: mouseX,
-                        y: mouseY
-                    }
+                    gradient = mouseGradientPoint
                 }
 
                 let distance = Math.sqrt(Math.pow(gradient.x - curr.x, 2) + Math.pow(gradient.y - curr.y, 2))
-
                 let oMaxReach = maxReach
                 if (z == -1) {
                     oMaxReach *= 1
                 }
 
-                if (distance > oMaxReach) continue;
+                if (distance > oMaxReach * 1.2) continue;
 
                 let influence = 1 - (distance / oMaxReach);
 
@@ -83,6 +84,7 @@ const updatePoints = () => {
             let avgX = 0;
             let avgY = 0;
             for (let z = 0; z < gradientsToTakeIntoAccount.length; z++) {
+
                 let offsetX = curr.x - gradientsToTakeIntoAccount[z].gradient.x
                 let offsetY = curr.y - gradientsToTakeIntoAccount[z].gradient.y
                 offsetX *= Math.pow(gradientsToTakeIntoAccount[z].influence, 2)
@@ -92,6 +94,15 @@ const updatePoints = () => {
             }
             avgX /= gradientsToTakeIntoAccount.length
             avgY /= gradientsToTakeIntoAccount.length
+
+            let pressure = Math.sqrt(Math.pow(avgX, 2) + Math.pow(avgY, 2))
+            pressure = Math.pow(pressure, 10)
+            pressure /= 10
+            
+            const lerp = (a, b) => {
+                return (b - a) * pressure + a
+            }
+            points[atIndex].color = `rgb(${lerp(0, 255)},${lerp(255,0)},0)`
 
             const keepInBoundsFactor = 0.1
             const keepinBoundsRange = 0
@@ -132,14 +143,15 @@ const placePoints = (columns) => {
                 x: (x / (columnsX - 1)) * size.width,
                 y: (y / (columnsY - 1)) * size.height,
                 velocityX: 0,
-                velocityY: 0
+                velocityY: 0,
+                color: "white"
             })
         }
     }
 }
 
 const reloadPoints = () => {
-    placePoints(20)
+    placePoints(40)
     size = {
         width: window.innerWidth,
         height: window.innerHeight
